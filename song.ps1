969a9df386d2dbf7aae5861fa311567869af702e
Jacob-CGI <80006006+Jacob-CGI@users.noreply.github.com>
2021-03-11 11:30:23 -0500

    Add files via upload

---
+++ song.ps1
@@ -0,0 +1,11 @@
+[console]::beep(523,250)
+[console]::beep(523,250)
+[console]::beep(523,250)
+[console]::beep(523,500)
+
+[console]::beep(415,500)
+[console]::beep(466,500)
+
+[console]::beep(523,500)
+[console]::beep(466,250)
+[console]::beep(523,1000)
